13b4c80e0204edd5f8fc01913c00a7826bd20529
Sneha <sneha.chenna08@gmail.com>
2024-03-20 21:34:53 +0100

    BaseProject Created

---
+++ src/Test2.java
@@ -0,0 +1,7 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("TEST2");
+        System.out.println("TEST222");
+        System.out.println("TEST2222222");
+    }
+}
---
+++ src/Test3.java
@@ -0,0 +1,6 @@
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("TEST3");
+        System.out.println("TEST3333");
+    }
+}
