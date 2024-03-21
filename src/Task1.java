67501435a01bb029696b0684051331d6d85ef0cc
Sneha <sneha.chenna08@gmail.com>
2024-03-21 14:28:50 +0100

    Have a nice day

---
+++ src/Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Java programming");
+        System.out.println("Developer");
+        System.out.println("Coding");
+    }
+}
